7c1f54bd911159281cefcd20e1c6424b0aee2dc6
hotdog2000 <pcasgrain@gmail.com>
2018-10-19 18:14:08 -0400

    Code Cleanup

--- NashRL_Lib.py
+++ NashRL_Lib.py
@@ -1,16 +1,9 @@
-import numpy as np
-import math
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-# import torchvision.transforms as T
-from torch.nn.parameter import Parameter
 
 
 class DQN(nn.Module):
-    def __init__(self, input_dim, output_dim, num_players = 3):
+    def __init__(self, input_dim, output_dim, num_players=3):
         super(DQN, self).__init__()
         self.num_players = num_players
         # Define basic fully connected network for parameters in Advantage function
@@ -48,49 +41,15 @@ class DQN(nn.Module):
         )
 
 
-class BlockPermInvariantLinear(torch.nn.Module):
-    """
-    This  an nn.Linear layer that is invariant to block permutations of the input.
-    Input is `in_features`-Dimensional and Output is `out_features`-dimensional.
-    Assume input is composed of blocks [A1,A2,...,An] each of size `block_size`.
-    By default, `block_size` is assumed to be 1..
-    """
-
-    def __init__(self, in_features, out_features, block_size=1, bias=True):
-
-        super(BlockPermInvariantLinear, self).__init__()
-
-        # Store input and output dimensions
-        self.in_features = in_features
-        self.out_features = out_features
-        self.block_size = block_size
-
-        assert (self.in_features) % (self.block_size), "in_features must be a multiple of block size."
-
-        # Generate one row of rank-1 matrix, store as parameter, as well as bias
-        # Register both as parameters
-        self.weight = Parameter(torch.Tensor(out_features, block_size))
-        if bias:
-            self.bias = Parameter(torch.Tensor(out_features))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
-
-        # Define full matrix used in linear transformation, uses repeated single row
-        self.rep_weight = self.weight.repeat(1, self.in_features / self.block_size)
-
-    def forward(self, x):
-        return F.linear(input, self.rep_weight, self.bias)
-
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
-
-
 class PermInvariantQNN(torch.nn.Module):
-    def __init__(self, in_invar_dim, non_invar_dim, out_dim, block_size=1, num_moments=5):
+    block_size: int
+    in_invar_dim: int
+    non_invar_dim: int
+    num_moments: int
+    out_dim: int
+
+    def __init__(self, in_invar_dim, non_invar_dim,
+                 out_dim, block_size=1, num_moments=5):
         super(PermInvariantQNN, self).__init__()
 
         # Store input and output dimensions
@@ -101,7 +60,7 @@ class PermInvariantQNN(torch.nn.Module):
         self.out_dim = out_dim
 
         # Verify invariant dimension is multiple of block size
-        assert (self.in_invar_dim) % (self.block_size), "in_invar_dim must be a multiple of block size."
+        assert self.in_invar_dim % self.block_size, "in_invar_dim must be a multiple of block size."
 
         # Compute Number of blocks
         self.num_blocks = self.in_invar_dim / self.block_size
