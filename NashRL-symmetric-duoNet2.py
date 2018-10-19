6806c0a903fbc622f6dd95af8f0fde8b00a668c2
hotdog2000 <pcasgrain@gmail.com>
2018-10-19 15:40:30 -0400

    Re-Structuring

--- NashRL-symmetric-duoNet2.py
+++ NashRL-symmetric-duoNet2.py
@@ -13,6 +13,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 #import torchvision.transforms as T
 
+from simulation_lib import *
+from NashRL_Lib import *
+
 # -------------------------------------------------------------------
 # This file defines some of the necessary objects and classes needed
 # for the LQ-Nash Reinforcement Learning Algorithm
@@ -29,8 +32,10 @@ Transition = namedtuple('Transition',
 # ***NOTE*** All elements are tensors
 class FittedValues(object):
     # Initialized via a single numpy vector
-    def __init__(self,value_vector,v_value):
-        self.num_players = 3 #number of players in game
+    def __init__(self,value_vector,v_value,num_players):
+
+        self.num_players = num_players #number of players in game
+
         self.V = v_value #nash value vector
         
         self.mu = value_vector[0:self.num_players] #mean of each player
@@ -107,7 +112,9 @@ class DQN(nn.Module):
         return self.main(input), self.main_V(input)
     
 class NashNN():
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, num_players = 3):
+        self.num_players = num_players
+
         self.main_net = DQN(input_dim, output_dim)
         self.target_net = copy.deepcopy(self.main_net)
         self.num_sim = 10000
@@ -132,7 +139,7 @@ class NashNN():
         
     # Transforms output tensor into FittedValues Object
     def tensorTransform(self, output1, output2):
-        return FittedValues(output1, output2)
+        return FittedValues(output1, output2, self.num_players)
     
     def updateLearningRate(self):
         self.counter += 1
@@ -191,95 +198,6 @@ class ExperienceReplay:
         return random.sample(self.buffer,size)
 
 # Define Market Game Simulation Object
-class MarketSimulator(object):
-    def __init__(self,param_dict):
-
-        # Unpack Parameter Dictionary
-
-        # Game-Specific Parameters
-        self.p_imp  = param_dict['price_impact']
-        self.t_cost = param_dict['transaction_cost']
-        self.L_cost = param_dict['liquidation_cost']
-        self.phi    = param_dict['running_penalty']
-
-        self.T      = param_dict['T']
-        self.dt     = param_dict['dt']
-
-        self.N      = param_dict['N_agents']
-
-        # Simulation Parameters
-        self.mu      = param_dict['drift_function']
-        self.sigma   = param_dict['volatility']
-
-        # Define Reward Functions for t<T and t=T (Revise, perhaps)
-        self.r  = lambda Q,S,nu : - nu*(S + self.t_cost*nu) - self.phi * Q**2
-        self.rT = lambda Q,S,nu : Q*(S - Q*self.L_cost)
-
-        # Allocating Memory for Game Variables
-        self.Q = np.random.normal(0,10,self.N)
-        self.S = np.float32(10+np.random.normal(0,self.sigma))
-        self.dS = np.float32(0)
-        self.dF = np.float32(0)
-        # self.F = np.float32(0)
-        self.t = np.float32(0)
-
-        # Variable Containing Total Accumulated Score
-        self.last_reward = np.zeros( self.N, dtype=np.float32 )
-        self.total_reward = np.zeros( self.N, dtype=np.float32 )
-
-        # Variable Containing BM increments
-        self.dW = np.random.normal(0, np.sqrt(self.dt),
-                                      int(round(np.ceil(self.T / self.dt) + 2 )))
-
-        # Variable Indicating Whether Done
-        self.done = False
-
-    def reset(self):
-        # Reset Game Values
-        self.Q = np.random.normal(0,10,self.N)
-        self.S = np.float32(10+np.random.normal(0,self.sigma))
-        self.t = np.float32(0)
-
-        self.last_reward = np.zeros( self.N, dtype=np.float32 )
-        self.total_reward = np.zeros(self.N, dtype=np.float32)
-
-        self.dW = np.random.normal(0, np.sqrt(self.dt),
-                                      int(round(np.ceil(self.T / self.dt) + 2 )))
-
-        self.done = False
-
-    def step(self,nu):
-
-        last_state = (self.Q,self.S)
-
-        if self.t < self.T:
-            # Advance Inventory & Time
-            self.Q += nu
-            self.t += self.dt
-
-            # Compute Action Reward
-            self.last_reward = self.r(self.Q,self.S,nu)
-            self.total_reward += self.last_reward
-
-            # Advance Asset Price
-            self.dF = self.mu(self.t,self.S) * self.dt + self.sigma * self.dW[int(round(self.t))]
-            self.dS = self.dF + self.dt * ( self.p_imp * np.mean(nu) )
-            self.S += self.dS
-
-#        elif (not self.done):
-#
-#            # Compute Action Reward
-#            self.last_reward = self.rT(self.Q, self.S, self.nu)
-#            self.total_reward += self.last_reward
-#
-#            # Update Variables
-#            self.Q = np.zeros(self.N, dtype=np.float32)
-
-        return Transition( last_state, nu, (self.Q,self.S), self.last_reward )
-
-    def get_state(self):
-        return (self.Q,self.S), self.last_reward, self.total_reward
-    
 if __name__ == '__main__':
     num_players = 3
     T = 3
--- simulation_lib.py
+++ simulation_lib.py
@@ -1,1 +1,90 @@
+import numpy as np
 
+class MarketSimulator(object):
+    def __init__(self,param_dict):
+
+        # Unpack Parameter Dictionary
+
+        # Game-Specific Parameters
+        self.p_imp  = param_dict['price_impact']
+        self.t_cost = param_dict['transaction_cost']
+        self.L_cost = param_dict['liquidation_cost']
+        self.phi    = param_dict['running_penalty']
+
+        self.T      = param_dict['T']
+        self.dt     = param_dict['dt']
+
+        self.N      = param_dict['N_agents']
+
+        # Simulation Parameters
+        self.mu      = param_dict['drift_function']
+        self.sigma   = param_dict['volatility']
+
+        # Define Reward Functions for t<T and t=T (Revise, perhaps)
+        self.r  = lambda Q,S,nu : - nu*(S + self.t_cost*nu) - self.phi * Q**2
+        self.rT = lambda Q,S,nu : Q*(S - Q*self.L_cost)
+
+        # Allocating Memory for Game Variables
+        self.Q = np.random.normal(0,10,self.N)
+        self.S = np.float32(10+np.random.normal(0,self.sigma))
+        self.dS = np.float32(0)
+        self.dF = np.float32(0)
+        # self.F = np.float32(0)
+        self.t = np.float32(0)
+
+        # Variable Containing Total Accumulated Score
+        self.last_reward = np.zeros( self.N, dtype=np.float32 )
+        self.total_reward = np.zeros( self.N, dtype=np.float32 )
+
+        # Variable Containing BM increments
+        self.dW = np.random.normal(0, np.sqrt(self.dt),
+                                      int(round(np.ceil(self.T / self.dt) + 2 )))
+
+        # Variable Indicating Whether Done
+        self.done = False
+
+    def reset(self):
+        # Reset Game Values
+        self.Q = np.random.normal(0,10,self.N)
+        self.S = np.float32(10+np.random.normal(0,self.sigma))
+        self.t = np.float32(0)
+
+        self.last_reward = np.zeros( self.N, dtype=np.float32 )
+        self.total_reward = np.zeros(self.N, dtype=np.float32)
+
+        self.dW = np.random.normal(0, np.sqrt(self.dt),
+                                      int(round(np.ceil(self.T / self.dt) + 2 )))
+
+        self.done = False
+
+    def step(self,nu):
+
+        last_state = (self.Q,self.S)
+
+        if self.t < self.T:
+            # Advance Inventory & Time
+            self.Q += nu
+            self.t += self.dt
+
+            # Compute Action Reward
+            self.last_reward = self.r(self.Q,self.S,nu)
+            self.total_reward += self.last_reward
+
+            # Advance Asset Price
+            self.dF = self.mu(self.t,self.S) * self.dt + self.sigma * self.dW[int(round(self.t))]
+            self.dS = self.dF + self.dt * ( self.p_imp * np.mean(nu) )
+            self.S += self.dS
+
+#        elif (not self.done):
+#
+#            # Compute Action Reward
+#            self.last_reward = self.rT(self.Q, self.S, self.nu)
+#            self.total_reward += self.last_reward
+#
+#            # Update Variables
+#            self.Q = np.zeros(self.N, dtype=np.float32)
+
+        return Transition( last_state, nu, (self.Q,self.S), self.last_reward )
+
+    def get_state(self):
+        return (self.Q,self.S), self.last_reward, self.total_reward
